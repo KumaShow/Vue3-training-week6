7895a8177c02f14daf0e7a7bd1858a610b3eb47a
KumaShow <kumadth@gmail.com>
2022-03-14 22:43:30 +0800

    新增後台DashboardProductModal

---
+++ src/components/DashboardProductModal.vue
@@ -0,0 +1,156 @@
+<template>
+    <div id="dashboardProductModal" ref="dashboardProductModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
+                aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content border-0">
+                    <div class="modal-header bg-dark text-white">
+                        <h5 id="productModalLabel" class="modal-title">
+                            <span>新增產品</span>
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="mb-2">
+                                    <div class="mb-3" id="newImg">
+                                        <label for="imageUrl" class="form-label">輸入圖片網址</label>
+                                        <input v-model="tempProduct.imageUrl" type="text" class="form-control"
+                                            placeholder="請輸入圖片連結">
+                                    </div>
+                                    <img class="img-fluid" :src="tempProduct.imageUrl" alt="">
+                                </div>
+                                <div class="mb-3">
+                                    <h3>多圖設置</h3>
+                                    <!-- 判斷是否為陣列 -->
+                                    <div v-if="Array.isArray(tempProduct.imagesUrl)">
+                                        <div v-for="(img, key) in tempProduct.imagesUrl" :key="key + '123'">
+                                            <input v-model="tempProduct.imagesUrl[key]" type="text" class="form-control"
+                                                placeholder="請輸入圖片連結">
+                                            <img class="img-fluid" :src="tempProduct.imagesUrl[key]" alt="">
+                                        </div>
+                                        <!-- v-if 先判斷陣列有沒有內容 || 再判斷 陣列特定索引位置 是否有輸入文字 -->
+                                        <!-- 當有文字的時候可以新增下一筆 -->
+                                        <button
+                                            v-if="!tempProduct.imagesUrl.length || tempProduct.imagesUrl[tempProduct.imagesUrl.length -1]"
+                                            class="btn btn-outline-primary btn-sm d-block w-100"
+                                            @click="tempProduct.imagesUrl.push('')">
+                                            新增圖片
+                                        </button>
+
+                                        <button v-else class="btn btn-outline-danger btn-sm d-block w-100"
+                                            @click="tempProduct.imagesUrl.pop('')">
+                                            刪除最後一張圖片
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="mb-3">
+                                    <label for="title" class="form-label">標題</label>
+                                    <input id="title" v-model="tempProduct.title" type="text" class="form-control"
+                                        placeholder="請輸入標題">
+                                </div>
+
+                                <div class="row">
+                                    <div class="mb-3 col-md-6">
+                                        <label for="category" class="form-label">分類</label>
+                                        <input id="category" v-model="tempProduct.category" type="text"
+                                            class="form-control" placeholder="請輸入分類">
+                                    </div>
+                                    <div class="mb-3 col-md-6">
+                                        <label for="price" class="form-label">單位</label>
+                                        <input id="unit" v-model="tempProduct.unit" type="text" class="form-control"
+                                            placeholder="請輸入單位">
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="mb-3 col-md-6">
+                                        <label for="origin_price" class="form-label">原價</label>
+                                        <input id="origin_price" v-model.number="tempProduct.origin_price" type="number"
+                                            min="0" class="form-control" placeholder="請輸入原價">
+                                    </div>
+                                    <div class="mb-3 col-md-6">
+                                        <label for="price" class="form-label">售價</label>
+                                        <input id="price" v-model.number="tempProduct.price" type="number" min="0"
+                                            class="form-control" placeholder="請輸入售價">
+                                    </div>
+                                </div>
+                                <hr>
+
+                                <div class="mb-3">
+                                    <label for="description" class="form-label">產品描述</label>
+                                    <textarea id="description" v-model="tempProduct.description" type="text"
+                                        class="form-control" placeholder="請輸入產品描述">
+
+                                    </textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="content" class="form-label">說明內容</label>
+                                    <textarea id="content" v-model="tempProduct.content" type="text"
+                                        class="form-control" placeholder="請輸入說明內容">
+
+                                    </textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <div class="form-check">
+                                        <input id="is_enabled" v-model="tempProduct.is_enabled" class="form-check-input"
+                                            type="checkbox" :true-value="1" :false-value="0">
+                                        <label class="form-check-label" for="is_enabled">是否啟用</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                            取消
+                        </button>
+                        <button type="button" class="btn btn-primary" @click="updateProduct">
+                            確認
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+import Modal from 'bootstrap/js/dist/modal'
+
+export default {
+  data () {
+    return {
+      modal: {},
+      tempProduct: {}
+    }
+  },
+  //   props: ['tempProduct', 'isNew'],
+  methods: {
+    // 當彈出視窗元件化後，要把方法綁訂到元件內使用
+    updateProduct () {
+      let url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/product`
+      let method = 'post'
+
+      if (!this.isNew) {
+        url = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/product/${this.tempProduct.id}`
+        method = 'put'
+      }
+      // 依據api文件須回傳data物件格式
+      this.$http[method](url, { data: this.tempProduct })
+        .then((res) => {
+          // 觸發外層方法getProducts
+          this.$emit('get-products')
+          this.modal.hide()
+        })
+        .catch((err) => {
+          alert(err.data.message)
+        })
+    }
+  },
+  mounted () {
+    this.modal = new Modal(this.$refs.modal)
+  }
+}
+</script>
