b36c7e9347f31355544e4db3e78c122682bb1670
KumaShow <kumadth@gmail.com>
2022-05-30 17:57:45 +0800

    deploy

---
+++ js/about.33b0d988.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkuma_cake"]=self["webpackChunkkuma_cake"]||[]).push([[443],{7381:function(a,n,u){u.r(n),u.d(n,{default:function(){return f}});var e=u(3396);const t={class:"about"},c=(0,e._)("h1",null,"This is an about page",-1),r=[c];function s(a,n){return(0,e.wg)(),(0,e.iD)("div",t,r)}var i=u(89);const k={},o=(0,i.Z)(k,[["render",s]]);var f=o}}]);
+//# sourceMappingURL=about.33b0d988.js.map
